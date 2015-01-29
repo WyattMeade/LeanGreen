beea8ea2a94ab525af3bdd1fb77c73e2a73b7c38
KennethJackson <kenneth@whoiskenjackson.com>
2015-01-28 19:04:31 -0800

    updating about page

--- website/wp-content/themes/leangreen/inc/page-css.php
+++ website/wp-content/themes/leangreen/inc/page-css.php
@@ -16,22 +16,34 @@
  */
 function page_specific_css() {
     
-    /* Homepage CSS */
     if ( is_page( 'home' ) ) {
         wp_register_style( 'home_css', get_template_directory_uri().'/css/pages/home.css' );
         wp_enqueue_style( 'home_css' );
     }
-    
-    /* Homepage CSS */
+
     if ( is_page( 'about' ) ) {
         wp_register_style( 'about_css', get_template_directory_uri().'/css/pages/about.css' );
         wp_enqueue_style( 'about_css' );
     }
-    
-    /* Homepage CSS */
+
     if ( is_page( 'contact-us' ) ) {
         wp_register_style( 'contact_css', get_template_directory_uri().'/css/pages/contact-us.css' );
         wp_enqueue_style( 'contact_css' );
+    }
+
+    if ( is_page( 'testimonials' ) ) {
+        wp_register_style( 'testimonials_css', get_template_directory_uri().'/css/pages/testimonials.css' );
+        wp_enqueue_style( 'testimonials_css' );
+    }
+    
+    if ( is_page( 'vending-solutions' ) ) {
+        wp_register_style( 'vendingsolutions_css', get_template_directory_uri().'/css/pages/solutions.css' );
+        wp_enqueue_style( 'vendingsolutions_css' );
+    }
+    
+    if ( is_page( 'who-we-serve' ) ) {
+        wp_register_style( 'whoweserve_css', get_template_directory_uri().'/css/pages/serve.css' );
+        wp_enqueue_style( 'whoweserve_css' );
     }
 
 }

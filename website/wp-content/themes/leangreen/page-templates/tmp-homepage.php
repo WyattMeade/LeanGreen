f8bd7cd38f15d380489a535d9ae3b5799a2888e5
KennethJackson <kenneth@whoiskenjackson.com>
2015-02-15 13:02:46 -0800

    updating home page layouts

--- website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
+++ website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<section class="homeMain clearfix">
 		<div class="main">
-			<div class="large_11 col centered">
+			<div class="large_11 small_15 col centered">
 				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_header.png">
 				<h1>Changing the Chicago vending landscape.</h1>
 				<p>LeanGreen Vending is Chicago's standard on how vending ought to be. We're what happens when you blend good ol' fashioned customer care with the latest and greatest vending solutions. Our laser-like focus is on customer service and product selection. When it comes to product options, Leangreen stands apart from the other companies because of our emphasis on healthier options.</p>
@@ -22,10 +22,12 @@ get_header(); ?>
 	</section>
 	<section class="homeOne clearfix">
 		<div class="main">
-			<div class="large_8 col pull-right">
-				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_whoweserve.png">
+			<div class="large_8 small_15 col pull-right table">
+				<div class="table-cell">
+					<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_whoweserve.png">
+				</div>
 			</div>
-			<div class="large_7 col table">
+			<div class="large_7 small_15 col table">
 				<div class="table-cell">
 					<h1 class="colorGreen">To Serve and Vend</h1>
 					<p>Whether you're a school, office, health club, hospital, park district, YMCA, etc... our mission is to vend like no one has vended before! We have a broad range of customers, each unique in their vending needs. That's why we go to great lengths to listen to what you're looking for and then deliver the best fit of equipment and products to meet your needs.</p>
@@ -36,10 +38,12 @@ get_header(); ?>
 	</section>
 	<section class="homeTwo clearfix">
 		<div class="main">
-			<div class="large_8 col">
-				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_testimonials.png">
+			<div class="large_8 small_15 col table">
+				<div class="table-cell">
+					<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_testimonials.png">
+				</div>
 			</div>
-			<div class="large_7 col table">
+			<div class="large_7 small_15 col table">
 				<div class="table-cell">
 					<h1 class="colorPurple">A Friendly Face Behind Every Machine</h1>
 					<p>Is the vending status quo as good as it gets? Nope. At LeanGreen Vending, we listen and we deliver. Our Customer Service is unparalleled. We could go on and on about why our service is different but our customers say it so much better.</p>
@@ -50,10 +54,10 @@ get_header(); ?>
 	</section>
 	<section class="homeOne clearfix">
 		<div class="main">
-			<div class="large_8 col pull-right">
+			<div class="large_8 small_15 col pull-right table">
 				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_vendingsolutions.png">
 			</div>
-			<div class="large_7 col table">
+			<div class="large_7 small_15 col table">
 				<div class="table-cell">
 					<h1 class="colorGold">Something' For Everyone</h1>
 					<p>Snack machines? Check. Cold drink machines? Ditto. Coffee? You bet. Micro-Markets? Piece of cake. Piece of Cake machine? Um... no. But we do offer ice cream vending! Whatever your location needs, we can provide modern, attractive equipment and a better (and broader) product selection.</p>
@@ -64,10 +68,10 @@ get_header(); ?>
 	</section>
 	<section class="homeTwo clearfix">
 		<div class="main">
-			<div class="large_8 col">
+			<div class="large_8 small_15 col table">
 				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_aboutus.png">
 			</div>
-			<div class="large_7 col table">
+			<div class="large_7 small_15 col table">
 				<div class="table-cell">
 					<h1 class="colorLtGreen">Humble Snack Beginnings</h1>
 					<p>We started with just one vending machine in 2011. In less than 3 years, we now cover all of Greater Chicago. Click here to read more about our humble start and how we got this far this fast - with class.</p>
@@ -78,10 +82,10 @@ get_header(); ?>
 	</section>
 	<section class="homeOne clearfix">
 		<div class="main">
-			<div class="large_8 col pull-right">
+			<div class="large_8 small_15 col pull-right table">
 				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_contactus.png">
 			</div>
-			<div class="large_7 col table">
+			<div class="large_7 small_15 col table">
 				<div class="table-cell">
 					<h1 class="colorOrange">Give Us A Shout</h1>
 					<p>Getting set up with our vending service couldn't be easier. We start with a chat, follow that up with a visit, and it's gravy (low-fat gravy of course) after that. Whatever form of communication you prefer, we look forward to starting (or continuing) the conversation with you.</p>

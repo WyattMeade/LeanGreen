b051bc90642540fd0dbed1b900058324b54f1089
KennethJackson <kenneth@whoiskenjackson.com>
2015-02-16 21:49:52 -0800

    updating homepage buttons and theme treatment

--- website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
+++ website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
@@ -54,10 +54,11 @@ get_header(); ?>
 		$homepageTitle = $custom_fields['homepage_title'][0];
 		$homepageExcerpt = $custom_fields['homepage_excerpt'][0];
 		$homepageButtonText = $custom_fields['homepage_button_text'][0];
+		$homepageColorScheme = $custom_fields['homepage_color_scheme'][0];
 
 		?>
 
-		<section class="section <?php if($count == 0) { echo $altBG; } ?> clearfix">
+		<section class="section theme-<?php if($homepageColorScheme){ echo $homepageColorScheme; } ?> <?php if($count == 0) { echo $altBG; } ?> clearfix">
 			<div class="main">
 				<div class="large_8 small_15 col <?php if($count == 0) { echo $pullRight; } ?> table">
 					<div class="table-cell">
@@ -68,9 +69,9 @@ get_header(); ?>
 				</div>
 				<div class="large_7 small_15 col table">
 					<div class="table-cell">
-						<h1 class="colorGreen"><?php if($homepageTitle){ echo $homepageTitle; } ?></h1>
+						<h1><?php if($homepageTitle){ echo $homepageTitle; } ?></h1>
 						<p><?php if($homepageExcerpt){ echo $homepageExcerpt; } ?></p>
-						<button><span class="bgGreen"><?php if($homepageButtonText){ echo $homepageButtonText; } ?></span></button>
+						<a class="btn" href="<?php the_permalink(); ?>"><span><?php if($homepageButtonText){ echo $homepageButtonText; } ?></span></a>
 					</div>
 				</div>
 			</div>
@@ -91,82 +92,5 @@ get_header(); ?>
 	<?php endwhile; ?>
 
 	<?php wp_reset_query(); ?>
-
-	<!--
-	<section class="homeOne clearfix">
-		<div class="main">
-			<div class="large_8 small_15 col pull-right table">
-				<div class="table-cell">
-					<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_whoweserve.png">
-				</div>
-			</div>
-			<div class="large_7 small_15 col table">
-				<div class="table-cell">
-					<h1 class="colorGreen">To Serve and Vend</h1>
-					<p>Whether you're a school, office, health club, hospital, park district, YMCA, etc... our mission is to vend like no one has vended before! We have a broad range of customers, each unique in their vending needs. That's why we go to great lengths to listen to what you're looking for and then deliver the best fit of equipment and products to meet your needs.</p>
-					<button><span class="bgGreen">Who We Serve</span></button>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="homeTwo clearfix">
-		<div class="main">
-			<div class="large_8 small_15 col table">
-				<div class="table-cell">
-					<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_testimonials.png">
-				</div>
-			</div>
-			<div class="large_7 small_15 col table">
-				<div class="table-cell">
-					<h1 class="colorPurple">A Friendly Face Behind Every Machine</h1>
-					<p>Is the vending status quo as good as it gets? Nope. At LeanGreen Vending, we listen and we deliver. Our Customer Service is unparalleled. We could go on and on about why our service is different but our customers say it so much better.</p>
-					<button><span class="bgPurple">Testimonials</span></button>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="homeOne clearfix">
-		<div class="main">
-			<div class="large_8 small_15 col pull-right table">
-				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_vendingsolutions.png">
-			</div>
-			<div class="large_7 small_15 col table">
-				<div class="table-cell">
-					<h1 class="colorGold">Something' For Everyone</h1>
-					<p>Snack machines? Check. Cold drink machines? Ditto. Coffee? You bet. Micro-Markets? Piece of cake. Piece of Cake machine? Um... no. But we do offer ice cream vending! Whatever your location needs, we can provide modern, attractive equipment and a better (and broader) product selection.</p>
-					<button><span class="bgGold">Vending Solutions</span></button>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="homeTwo clearfix">
-		<div class="main">
-			<div class="large_8 small_15 col table">
-				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_aboutus.png">
-			</div>
-			<div class="large_7 small_15 col table">
-				<div class="table-cell">
-					<h1 class="colorLtGreen">Humble Snack Beginnings</h1>
-					<p>We started with just one vending machine in 2011. In less than 3 years, we now cover all of Greater Chicago. Click here to read more about our humble start and how we got this far this fast - with class.</p>
-					<button><span class="bgLtGreen">About Us</span></button>
-				</div>
-			</div>
-		</div>
-	</section>
-	<section class="homeOne clearfix">
-		<div class="main">
-			<div class="large_8 small_15 col pull-right table">
-				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_contactus.png">
-			</div>
-			<div class="large_7 small_15 col table">
-				<div class="table-cell">
-					<h1 class="colorOrange">Give Us A Shout</h1>
-					<p>Getting set up with our vending service couldn't be easier. We start with a chat, follow that up with a visit, and it's gravy (low-fat gravy of course) after that. Whatever form of communication you prefer, we look forward to starting (or continuing) the conversation with you.</p>
-					<button><span class="bgOrange">Contact Us</span></button>
-				</div>
-			</div>
-		</div>
-	</section>
-	-->
 
 <?php get_footer(); ?>

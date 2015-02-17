9b51993a2e1230193883a2ef717525cfcb15d853
KennethJackson <kenneth@whoiskenjackson.com>
2015-02-16 21:16:02 -0800

    Modifying homepage template. Pulling in actual content. Modifying CSS for homepage sections.

--- website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
+++ website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
@@ -9,24 +9,90 @@
 
 get_header(); ?>
 
-<?php $the_query = new WP_Query( 'page_id=6' ); while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+	<?php $pageID = $page_id; ?>
 
-		<section class="homeMain clearfix">
+	<section class="homeMain clearfix">
 		<div class="main">
 			<div class="large_11 small_15 col centered">
 				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_header.png">
-				<h1><?php the_field('page_title'); ?></h1>
-				<?php the_content(); ?>
-				<h3>Dig a little deeper</h3>
+
+				<?php if ( have_posts() ) : 
+
+					// Start the loop.
+					while ( have_posts() ) : the_post();
+
+						the_content();
+
+					// End the loop.
+					endwhile;
+
+				endif; ?>
+				
 				<img src="<?php bloginfo( 'template_url' ); ?>/images/img_home_header_down.png">
 			</div>
 		</div>
 	</section>
 
+	<?php wp_reset_query(); ?>
+
+	<?php
+
+	$count = 0;
+	$altBG = 'alt-bg';
+	$pullRight = 'pull-right';
+	$homeList_query = new WP_Query( 'post_type=page&posts_per_page=-1&post_parent=0&order=ASC&orderby=menu_order' ); ?>
+
+	<?php while ( $homeList_query->have_posts() ) : $homeList_query->the_post(); ?>
+
+	<?php
+
+	if($post->ID != $pageID) {
+
+		/* Get the Custom Fields of the Post */
+		$custom_fields = get_post_custom();
+
+		$homepageTitle = $custom_fields['homepage_title'][0];
+		$homepageExcerpt = $custom_fields['homepage_excerpt'][0];
+		$homepageButtonText = $custom_fields['homepage_button_text'][0];
+
+		?>
+
+		<section class="section <?php if($count == 0) { echo $altBG; } ?> clearfix">
+			<div class="main">
+				<div class="large_8 small_15 col <?php if($count == 0) { echo $pullRight; } ?> table">
+					<div class="table-cell">
+						<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+							the_post_thumbnail();
+						} ?>
+					</div>
+				</div>
+				<div class="large_7 small_15 col table">
+					<div class="table-cell">
+						<h1 class="colorGreen"><?php if($homepageTitle){ echo $homepageTitle; } ?></h1>
+						<p><?php if($homepageExcerpt){ echo $homepageExcerpt; } ?></p>
+						<button><span class="bgGreen"><?php if($homepageButtonText){ echo $homepageButtonText; } ?></span></button>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<?php if($count > 0) {
+
+			$count = 0;
+
+		} else {
+
+			$count++;
+
+		} ?>
+
+	<?php } ?>
+
 	<?php endwhile; ?>
 
 	<?php wp_reset_query(); ?>
 
+	<!--
 	<section class="homeOne clearfix">
 		<div class="main">
 			<div class="large_8 small_15 col pull-right table">
@@ -101,5 +167,6 @@ get_header(); ?>
 			</div>
 		</div>
 	</section>
+	-->
 
 <?php get_footer(); ?>

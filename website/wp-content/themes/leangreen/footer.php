5c899fbb092f502505a4b14964ca4b1011e0e80b
WyattMeade <wyatt.meade@gmail.com>
2015-03-02 01:31:31 -0500

    Tweaked footer code; updated Responsiveness

--- website/wp-content/themes/leangreen/footer.php
+++ website/wp-content/themes/leangreen/footer.php
@@ -18,9 +18,14 @@
 			<div class="large_5 small_15 col social">
 				<p>Telephone: 888-947-8777</p>
 				<p><a href="#">info@leangreenhv.com</a></p>
-				<div>Follow us:&nbsp;</div><button><span><i class="icon-facebook-alt"></i></span></button><button><span><i class="icon-twitter"></i></span></button><button><span><i class="icon-pinterest"></i></span></button>
+				<span>Follow us:&nbsp;</span>
+				<div>
+					<button><a href="http://facebook.com" target="_blank" alt="Facebook"><span><i class="icon-facebook-alt"></i></span></a></button>
+					<button><a href="http://twitter.com" target="_blank" alt="Twitter"><span><i class="icon-twitter"></i></span></a></button>
+					<button><a href="http://pinterest.com" target="_blank" alt="Pinterest"><span><i class="icon-pinterest"></i></span></a></button>
+				</div>
 			</div>
-			<div class="large_4 small_14 col">
+			<div class="large_4 small_15 col">
 				<ul>
 					<li><a href="#">Privacy Policy</a></li>
 					<li><a href="#">Sitemap</a></li>

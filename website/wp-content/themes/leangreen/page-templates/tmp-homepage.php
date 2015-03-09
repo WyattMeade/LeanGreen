7e60ba8cb5f8357843cb9a1873f3b1cb18f406f3
WyattMeade <wyatt.meade@gmail.com>
2015-03-08 23:59:42 -0400

    Home Page CSS updates

--- website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
+++ website/wp-content/themes/leangreen/page-templates/tmp-homepage.php
@@ -63,7 +63,7 @@ get_header(); ?>
 				<div class="large_8 small_15 col <?php if($count == 0) { echo $pullRight; } ?> table">
 					<div class="table-cell">
 						<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-							the_post_thumbnail();
+							the_post_thumbnail('full');
 						} ?>
 					</div>
 				</div>

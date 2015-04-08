6d81c42641376f5f160ed0aa8b5dcddcad9ffeba
KennethJackson <kenneth@whoiskenjackson.com>
2015-04-07 22:11:21 -0700

    committing databases and config file.

--- website/wp-config.php
+++ website/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'leangreen');
+define('DB_NAME', 'leangreenwp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'leangreenwp');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'L3anGre3n!WP');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'leangreenwp.db.5985714.hostedresource.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
